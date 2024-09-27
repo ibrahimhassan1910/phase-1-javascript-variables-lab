ebd5a9983721aa577ce2a32c51dcad1aacb70aff
hassan <hassanibrahimndahiro@gmail.com>
2024-09-27 23:44:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+
+// Define a variable companyName as Scuber
+const companyName = 'Scuber';
+
+
+// Define a variable mostProfitableNeighborhood as Chelsea using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+
+// Define a variable companyCeo as Susan Smith using let
+let companyCeo = 'Susan Smith';
